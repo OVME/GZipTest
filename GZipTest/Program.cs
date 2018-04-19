eae31d6c25eea785b5253aa0b077d2f6737129ef
Ivan Salov <saloff666@gmail.com>
2018-04-20 00:57:10 +0700

    Add project files.

---
+++ GZipTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GZipTest
+{
+    public class Program
+    {
+        private const string CompressCommand = "compress";
+        private const string DecompressCommand = "decompress";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                ValidateParameters(args);
+            }
+            catch (Exception e)
+            {
+                WriteExceptionToConsole(e);
+                return;
+            }
+        }
+
+        private static void WriteExceptionToConsole(Exception e)
+        {
+            Console.Error.WriteLine(e.Message);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private static void ValidateParameters(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                throw new ArgumentException("Invalid parameters number");
+            }
+
+            if (args[0] != CompressCommand && args[0] != DecompressCommand)
+            {
+                throw new ArgumentException($"Invalid command parameter. Should be {CompressCommand} or {DecompressCommand}");
+            }
+        }
+    }
+}
