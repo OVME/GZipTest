56823f2e3b1db646c8d23d60b0a59c2bc1882837
Ivan Salov <saloff666@gmail.com>
2018-04-30 00:12:55 +0700

    implement MultiThreadCompressionWorker without proper error handling

--- GZipTest/Compress/MultiThreadCompressionWorker.cs
+++ GZipTest/Compress/MultiThreadCompressionWorker.cs
@@ -1,37 +1,132 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Threading;
 
 namespace GZipTest.Compress
 {
     public class MultiThreadCompressionWorker
     {
-        private const int BlockSize = 1048576;
-
         private readonly int _processorsCount;
 
-        private readonly object _locker = new object();
+        private readonly object _readLocker = new object();
 
-        private readonly FileStream _inputFileStream;
-
-        private readonly FileStream _outputFileStream;
+        private readonly object _writeLocker = new object();
 
         private int _blockNumber = 0;
 
         // TODO: handle exceptions in threads, mb make class instanse once-used
-        public MultiThreadCompressionWorker(FileStream inputFileStream, FileStream outputFileStream, int processorsCount)
+        public MultiThreadCompressionWorker(int processorsCount)
         {
-            _inputFileStream = inputFileStream;
-            _outputFileStream = outputFileStream;
             _processorsCount = processorsCount;
         }
 
-        public void Compress()
+        public void Compress(FileStream inputArchiveFileStream, FileStream outputFileStream)
         {
-            
+            WriteOriginalFileSize(inputArchiveFileStream, outputFileStream);
+
+            var threads = new Thread[_processorsCount];
+
+            for (var i = 0; i < _processorsCount; i++)
+            {
+                var thread = new Thread(RunCompression);
+                threads[i] = thread;
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start(new RunCompressionParameters
+                {
+                    InputArchiveFileStream = inputArchiveFileStream,
+                    OutputFileStream = outputFileStream
+                });
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+        }
+
+        private void RunCompression(object obj)
+        {
+            var parameters = (RunCompressionParameters) obj;
+
+            var inputArchiveFileStream = parameters.InputArchiveFileStream;
+            var outputFileStream = parameters.OutputFileStream;
+
+            var dataToCompress = new byte[FormatConstants.BlockSize];
+            int numberOfBytesReadFromInputFileStream;
+            int blockNumber;
+
+            while (true)
+            {
+                lock (_readLocker)
+                {
+                    numberOfBytesReadFromInputFileStream = inputArchiveFileStream.Read(dataToCompress, 0, FormatConstants.BlockSize);
+
+                    if (numberOfBytesReadFromInputFileStream == 0)
+                    {
+                        break;
+                    }
+
+                    blockNumber = GetNextBlockNumber();
+                }
+
+                var compressedData = CompressDataBlock(dataToCompress, numberOfBytesReadFromInputFileStream);
+
+                var compressedDataLength = compressedData.Length;
+
+                lock (_writeLocker)
+                {
+                    WriteCompressedBlock(outputFileStream, blockNumber, compressedDataLength, compressedData);
+                }
+            }
         }
 
         private int GetNextBlockNumber()
         {
             return _blockNumber++;
+        }
+
+        private void WriteOriginalFileSize(FileStream inputArchiveFileStream, FileStream outputFileStream)
+        {
+            var originalFileSize = inputArchiveFileStream.Length;
+
+            var originalFileSizeBytes = BitConverter.GetBytes(originalFileSize);
+
+            outputFileStream.Write(originalFileSizeBytes, 0, sizeof(long));
+        }
+
+        private byte[] CompressDataBlock(byte[] dataToCompress, int numberOfBytesReadFromInputFileStream)
+        {
+            byte[] compressedData;
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var gZipStream = new GZipStream(memoryStream, CompressionMode.Compress))
+                {
+                    gZipStream.Write(dataToCompress, 0, numberOfBytesReadFromInputFileStream);
+                }
+
+                compressedData = memoryStream.ToArray();
+            }
+
+            return compressedData;
+        }
+
+        private void WriteCompressedBlock(FileStream outputFileStream, int blockNumber, int compressedDataLength,
+            byte[] compressedData)
+        {
+            outputFileStream.Write(BitConverter.GetBytes(blockNumber), 0, sizeof(int));
+            outputFileStream.Write(BitConverter.GetBytes(compressedDataLength), 0, sizeof(int));
+            outputFileStream.Write(compressedData, 0, compressedDataLength);
+        }
+
+        private class RunCompressionParameters
+        {
+            public FileStream InputArchiveFileStream { get; set; }
+
+            public FileStream OutputFileStream { get; set; }
         }
     }
 }
---
+++ GZipTest/FormatConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZipTest
+{
+    public static class FormatConstants
+    {
+        public const int BlockSize = 1048576;
+    }
+}
